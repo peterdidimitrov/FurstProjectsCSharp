399da2c91ce97d78ae152ea8aa48460d7fa88201
peterdidimitrov <petr_dimitrov@abv.bg>
2022-11-06 21:19:12 +0200

    Add one more solution
    Add solution of the last problem in Associative Arrays - Exercise - "07. Company Users"

--- C# Fundamentals/18.AssociativeArrays-Exercise/07.CompanyUsers/Program.cs
+++ C# Fundamentals/18.AssociativeArrays-Exercise/07.CompanyUsers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _07.CompanyUsers
 {
@@ -6,7 +8,40 @@ namespace _07.CompanyUsers
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var companies = new Dictionary<string, List<string>>();
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] strings = input
+                    .Split(" -> ", StringSplitOptions.RemoveEmptyEntries);
+                string companyName = strings[0];
+                string employeeId = strings[1];
+
+                if (!companies.ContainsKey(companyName))
+                {
+                    companies.Add(companyName, new List<string>());
+                    companies[companyName].Add(employeeId);
+                }
+                else
+                {
+                    if (companies[companyName].Contains(employeeId))
+                    {
+                        continue;
+                    }
+                    else
+                    {
+                        companies[companyName].Add(employeeId);
+                    }
+                }
+            }
+            foreach (var item in companies)
+            {
+                Console.WriteLine($"{item.Key}");
+                foreach (var employee in item.Value)
+                {
+                    Console.WriteLine($"-- {employee}");
+                }
+            }
         }
     }
 }
