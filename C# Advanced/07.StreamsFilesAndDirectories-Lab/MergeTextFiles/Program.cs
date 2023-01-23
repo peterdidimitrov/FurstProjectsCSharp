5d469b213db2425fcd42921745681bec2566e941
peterdidimitrov <petr_dimitrov@abv.bg>
2023-01-24 00:25:48 +0200

    Add new solutions

---
+++ C# Advanced/07.StreamsFilesAndDirectories-Lab/MergeTextFiles/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace MergeFiles
+{
+    using System;
+    using System.IO;
+    public class MergeFiles
+    {
+        static void Main()
+        {
+            var firstInputFilePath = @"..\..\..\Files\input1.txt";
+            var secondInputFilePath = @"..\..\..\Files\input2.txt";
+            var outputFilePath = @"..\..\..\Files\output.txt";
+
+            MergeTextFiles(firstInputFilePath, secondInputFilePath, outputFilePath);
+        }
+
+        public static void MergeTextFiles(string firstInputFilePath, string secondInputFilePath, string outputFilePath)
+        {
+            string[] textOne = File.ReadAllLines(firstInputFilePath);
+            string[] textTwo = File.ReadAllLines(secondInputFilePath);
+
+            using (StreamWriter writer = new StreamWriter(outputFilePath))
+            {
+                if (textOne.Length <= textTwo.Length)
+                {
+                    for (int i = 0; i < textOne.Length; i++)
+                    {
+                        writer.WriteLine(textOne[i]);
+                        writer.WriteLine(textTwo[i]);
+                    }
+                    for (int i = textOne.Length; i < textTwo.Length; i++)
+                    {
+                        writer.WriteLine(textTwo[i]);
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i < textTwo.Length; i++)
+                    {
+                        writer.WriteLine(textOne[i]);
+                        writer.WriteLine(textTwo[i]);
+                    }
+                    for (int i = textTwo.Length; i < textOne.Length; i++)
+                    {
+                        writer.WriteLine(textOne[i]);
+                    }
+                }
+            }
+        }
+    }
+}
