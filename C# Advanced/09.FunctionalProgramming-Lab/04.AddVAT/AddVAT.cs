0115f8dfcb0aaa698c2b32ca9118fbbc6b0704f4
peterdidimitrov <petr_dimitrov@abv.bg>
2023-01-26 14:55:38 +0200

    Add more solutions and make changes in other solutions

---
+++ C# Advanced/09.FunctionalProgramming-Lab/04.AddVAT/AddVAT.cs
@@ -0,0 +1,20 @@
+﻿namespace _04.AddVAT
+{
+    internal class AddVAT
+    {
+
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(new string[] { ", " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .Select(n => n *= 1.20)
+                .ToArray();
+
+            foreach (var num in numbers)
+            {
+                Console.WriteLine($"{num:f2}");
+            }
+        }
+    }
+}
---
+++ C# Advanced/09.FunctionalProgramming-Lab/05.FilterByAge/FilterByAge.cs
@@ -0,0 +1,67 @@
+﻿namespace _05.FilterByAge
+{
+    class FilterByAge
+    {
+        static void Main(string[] args)
+        {
+            Func<Person, int, bool> youngerFunc = (person, givenAge) => person.Age < givenAge;
+            Func<Person, int, bool> olderFunc = (person, givenAge) => person.Age >= givenAge;
+
+            int n = int.Parse(Console.ReadLine());
+            List<Person> allPersons = new List<Person>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] cmdArr = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string name = cmdArr[0];
+                int age = int.Parse(cmdArr[1]);
+
+                allPersons.Add(new Person(name, age));
+            }
+
+            string condition = Console.ReadLine();
+            int ageFilter = int.Parse(Console.ReadLine());
+            string[] printFilter = Console.ReadLine()
+                                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            switch (condition)
+            {
+                case "younger":
+                    allPersons = allPersons.Where(p => youngerFunc(p, ageFilter)).ToList();
+                    break;
+                case "older":
+                    allPersons = allPersons.Where(p => olderFunc(p, ageFilter)).ToList();
+                    break;
+            }
+
+            foreach (var person in allPersons)
+            {
+                List<string> output = new List<string>();
+                if (printFilter.Contains("name"))
+                {
+                    output.Add(person.Name);
+                }
+
+                if (printFilter.Contains("age"))
+                {
+                    output.Add(person.Age.ToString());
+                }
+
+                Console.WriteLine(string.Join(" - ", output));
+            }
+        }
+    }
+
+    internal class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
