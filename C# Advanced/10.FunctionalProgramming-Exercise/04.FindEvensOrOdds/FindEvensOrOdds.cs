84c4d662cb2e33995bc1faf4190e31ecd7fa3df8
peterdidimitrov <petr_dimitrov@abv.bg>
2023-01-27 15:15:49 +0200

    Add more solutions

---
+++ C# Advanced/10.FunctionalProgramming-Exercise/04.FindEvensOrOdds/FindEvensOrOdds.cs
@@ -0,0 +1,39 @@
+﻿namespace _04.FindEvensOrOdds
+{
+    internal class FindEvensOrOdds
+    {
+        static void Main(string[] args)
+        {
+
+            int[] range = Console.ReadLine()
+                 .Split()
+                 .Select(x => int.Parse(x))
+                 .ToArray();
+
+            int start = range[0];
+            int end = range[1];
+
+            List<int> numbers = new List<int>();
+
+            for (int i = start; i <= end; i++)
+            {
+                numbers.Add(i);
+            }
+
+            string type = Console.ReadLine();
+
+            Predicate<int> predicate = null;
+            
+
+            if (type == "even")
+            {
+                predicate = number => number % 2 == 0;
+            }
+            else if (type == "odd")
+            {
+                predicate = number => number % 2 != 0;
+            }
+            Console.WriteLine(string.Join(" ", numbers.FindAll(predicate)));
+        }
+    }
+}
---
+++ C# Advanced/10.FunctionalProgramming-Exercise/05.AppliedArithmetics/AppliedArithmetics.cs
@@ -0,0 +1,44 @@
+﻿namespace _05.AppliedArithmetics
+{
+    internal class AppliedArithmetics
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numbers = Console.ReadLine()
+                 .Split()
+                 .Select(x => decimal.Parse(x))
+                 .ToList();
+
+            Func<List<decimal>, List<decimal>> add = list => list
+            .Select(n => n += 1).ToList();
+            Func<List<decimal>, List<decimal>> multiply = list => list
+            .Select(n => n *= 2).ToList();
+            Func<List<decimal>, List<decimal>> subtract = list => list
+            .Select(n => n -= 1).ToList();
+
+            Action<List<decimal>> print = n => Console.WriteLine(string.Join(" ", numbers));
+
+            string operation;
+            while ((operation = Console.ReadLine()) != "end")
+            {
+                switch (operation)
+                {
+                    case "add":
+                        numbers = add(numbers);
+                        break;
+                    case "multiply":
+                        numbers = multiply(numbers);
+                        break;
+                    case "subtract":
+                        numbers = subtract(numbers);
+                        break;
+                    case "print":
+                        print(numbers);
+                        break;
+                    default:
+                        continue;
+                }
+            }
+        }
+    }
+}
