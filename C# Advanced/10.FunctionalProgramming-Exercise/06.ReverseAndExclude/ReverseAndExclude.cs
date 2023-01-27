71f6607bb6efde43724387db212f025902d51bcf
peterdidimitrov <petr_dimitrov@abv.bg>
2023-01-27 16:44:44 +0200

    Add some changes

---
+++ C# Advanced/10.FunctionalProgramming-Exercise/06.ReverseAndExclude/ReverseAndExclude.cs
@@ -0,0 +1,30 @@
+﻿namespace _06.ReverseAndExclude
+{
+    internal class ReverseAndExclude
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> numbers = Console.ReadLine()
+                 .Split()
+                 .Select(x => decimal.Parse(x))
+                 .ToList();
+
+            decimal devider = decimal.Parse(Console.ReadLine());
+
+            Predicate<decimal> predicate = x => x % devider == 0;
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (predicate(numbers[i]))
+                {
+                    numbers.Remove(numbers[i]);
+                    i--;
+                }
+            }
+            for (int i = numbers.Count - 1; i >= 0; i--)
+            {
+                Console.Write(numbers[i] + " ");
+            }
+        }
+    }
+}
