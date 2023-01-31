08af8db086972a23315f1f2e235574fe4827de17
peterdidimitrov <petr_dimitrov@abv.bg>
2023-01-31 14:41:11 +0200

    Make some changes

--- C# Advanced/11.DefiningClasses-Lab/CarManufacturer/Car.cs
+++ C# Advanced/11.DefiningClasses-Lab/CarManufacturer/Car.cs
@@ -8,36 +8,43 @@ namespace CarManufacturer
 {
     public class Car
     {
+        //fields
         private string make;
         private string model;
         private int year;
         private double fuelQuantity;
         private double fuelConsumption;
-        public string Make
+
+        //constructors
+        public Car()
         {
-            get { return make; }
-            set { make = value; }
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
         }
-        public string Model
+        public Car(string make, string model, int year) : this()
         {
-            get { return model; }
-            set { model = value; }
+            this.Make = make;
+            this.Model = model;
+            this.Year = year;
         }
-        public int Year
+        public Car(string make, string model, int year, double fuelQuantity, double fuelConsumption) : this(make, model, year)
         {
-            get { return year; }
-            set { year = value; }
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
         }
-        public double FuelQuantity
-        {
-            get { return fuelQuantity; }
-            set { fuelQuantity = value; }
-        }
-        public double FuelConsumption
-        {
-            get { return fuelConsumption; }
-            set { fuelConsumption = value; }
-        }
+
+
+        //propurties
+        public string Make { get { return make; } set { make = value; } }
+        public string Model { get { return model; } set { model = value; } }
+        public int Year { get { return year; } set { year = value; } }
+        public double FuelQuantity { get { return fuelQuantity; } set { fuelQuantity = value; } }
+        public double FuelConsumption { get { return fuelConsumption; } set { fuelConsumption = value; } }
+        
+        //methods
         public void Drive(double distance)
         {
             double needetfuel = distance * fuelConsumption;
--- C# Advanced/11.DefiningClasses-Lab/CarManufacturer/CarManufacturer.cs
+++ C# Advanced/11.DefiningClasses-Lab/CarManufacturer/CarManufacturer.cs
@@ -4,15 +4,15 @@
     {
         static void Main()
         {
-            Car car = new Car();
-
-            car.Make = "VW";
-            car.Model = "MK3";
-            car.Year = 1992;
-            car.FuelQuantity = 200;
-            car.FuelConsumption = 200;
-            car.Drive(200);
-            Console.WriteLine(car.WhoAmI());
+            string make = Console.ReadLine();
+            string model = Console.ReadLine();
+            int year = int.Parse(Console.ReadLine());
+            double fuelQuantity = double.Parse(Console.ReadLine());
+            double fuelConsuption = double.Parse(Console.ReadLine());
+            
+            Car firstCar = new Car();
+            Car secondCar = new Car(make, model, year);
+            Car thirdCar = new Car(make, model, year, fuelQuantity, fuelConsuption);
         }
     }
 }
