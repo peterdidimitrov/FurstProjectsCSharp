7c817c878e043bfded6a3ed371fc34340df29d76
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-02 15:07:30 +0200

    Add new solution

---
+++ C# Advanced/12.DefiningClasses-Exercise/04.OpinionPoll/Person.cs
@@ -0,0 +1,32 @@
+﻿namespace DefiningClasses;
+
+public class Person
+{
+    //Fields
+    private string name;
+    private int age;
+
+    //Constructors
+    public Person()
+    {
+        Name = "No name";
+        Age = 1;
+    }
+
+    public Person(int age)
+        : this()
+    {
+        Age = age;
+    }
+
+    public Person(string name, int age)
+        : this(age)
+    {
+        Name = name;
+    }
+
+    //Properties
+    public string Name { get; set; }
+
+    public int Age { get; set; }
+}
---
+++ C# Advanced/12.DefiningClasses-Exercise/04.OpinionPoll/StartUp.cs
@@ -0,0 +1,28 @@
+﻿namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        int count = int.Parse(Console.ReadLine());
+
+        var listOfPeople = new List<Person>();
+
+        for (int i = 0; i < count; i++)
+        {
+            string[] personProperties = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Person person = new(personProperties[0], int.Parse(personProperties[1]));
+
+            listOfPeople.Add(person);
+        }
+
+        var sortedListOfPeople = listOfPeople.Where(p => p.Age > 30).OrderBy(p => p.Name);
+
+        foreach (var person in sortedListOfPeople)
+        {
+            Console.WriteLine($"{person.Name} - {person.Age}");
+        }
+    }
+}
