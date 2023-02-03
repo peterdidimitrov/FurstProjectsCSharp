a7fa3cd30551cc97ca67fba5f1a3537f962a0b65
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-03 11:56:26 +0200

    Add one more solution

---
+++ C# Advanced/12.DefiningClasses-Exercise/07.RawData/Car.cs
@@ -0,0 +1,17 @@
+﻿namespace DefiningClasses
+{
+    public class Car
+    {
+        public Car(string model, Engine engine, Cargo cargo, Tire[] tires)
+        {
+            Model = model;
+            Engine = engine;
+            Cargo = cargo;
+            Tires = tires;
+        }
+        public string Model { get; set; }
+        public Engine Engine { get; set; }
+        public Cargo Cargo { get; set; }
+        public Tire[] Tires { get; set; }
+    }
+}
---
+++ C# Advanced/12.DefiningClasses-Exercise/07.RawData/StartUp.cs
@@ -0,0 +1,83 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Car> list = new();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] carInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = carInfo[0];
+
+                int engineSpeed = int.Parse(carInfo[1]);
+                int enginePower = int.Parse(carInfo[2]);
+
+                int cargoWeight = int.Parse(carInfo[3]);
+                string cargoType = carInfo[4];
+
+                double tire1Pressure = double.Parse(carInfo[5]);
+                int tire1Age = int.Parse(carInfo[6]);
+
+                double tire2Pressure = double.Parse(carInfo[7]);
+                int tire2Age = int.Parse(carInfo[8]);
+
+                double tire3Pressure = double.Parse(carInfo[9]);
+                int tire3Age = int.Parse(carInfo[10]);
+
+                double tire4Pressure = double.Parse(carInfo[11]);
+                int tire4Age = int.Parse(carInfo[12]);
+
+                Engine engine = new Engine(engineSpeed, enginePower);
+                Cargo cargo = new Cargo(cargoType, cargoWeight);
+                Tire[] tires = new Tire[4]
+                {
+                    new Tire(tire1Age, tire1Pressure),
+                    new Tire(tire2Age, tire2Pressure),
+                    new Tire(tire3Age, tire3Pressure),
+                    new Tire(tire4Age, tire4Pressure),
+                };
+
+                Car car = new Car(model, engine, cargo, tires);
+
+                list.Add(car);
+            }
+
+            string command = Console.ReadLine();
+            if (command == "fragile")
+            {
+                bool isFragile = false;
+                foreach (var car in list)
+                {
+                    foreach (var tire in car.Tires)
+                    {
+                        if (tire.Pressure < 1)
+                        {
+                            isFragile = true;
+                            break;
+                        }
+                    }
+                    if (isFragile == true)
+                    {
+                        Console.WriteLine(car.Model);
+                    }
+                }
+            }
+            else if (command == "flammable")
+            {
+                foreach (var car in list)
+                {
+                    if (car.Engine.Power > 250)
+                    {
+                        Console.WriteLine(car.Model);
+                    }
+                }
+            }
+        }
+    }
+}
