eda1b9cc19ee167c1d299b528da9c4ff9c914ded
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-04 21:59:36 +0200

    Add final solution of Car Salesman

--- C# Advanced/12.DefiningClasses-Exercise/08.CarSalesman/Engine.cs
+++ C# Advanced/12.DefiningClasses-Exercise/08.CarSalesman/Engine.cs
@@ -2,6 +2,10 @@
 {
     public class Engine
     {
+        public Engine()
+        {
+
+        }
         public Engine(string model, int power, int displacement, string efficiency)
         {
             Model = model;
@@ -31,5 +35,18 @@
         public int Power { get; set; }
         public int Displacement { get; set; }
         public string Efficiency { get; set; }
+        public override string ToString()
+        {
+            string displacement = Displacement == 0 ? "n/a" : Displacement.ToString();
+            string efficiency = Efficiency ?? "n/a";
+
+            string result =
+                $"{Model}:{Environment.NewLine}" +
+                $"    Power: {Power}{Environment.NewLine}" +
+                $"    Displacement: {displacement}{Environment.NewLine}" +
+                $"    Efficiency: {efficiency}";
+
+            return result;
+        }
     }
 }
--- C# Advanced/12.DefiningClasses-Exercise/08.CarSalesman/StartUp.cs
+++ C# Advanced/12.DefiningClasses-Exercise/08.CarSalesman/StartUp.cs
@@ -9,6 +9,8 @@
 
             int n = int.Parse(Console.ReadLine());
 
+            var engine = new Engine();
+
             for (int i = 0; i < n; i++)
             {
                 string[] engineInfo = Console.ReadLine()
@@ -16,12 +18,12 @@
 
                 if (engineInfo.Length == 4)
                 {
-                    Engine engine = new Engine(engineInfo[0], int.Parse(engineInfo[1]), int.Parse(engineInfo[2]), engineInfo[3]);
+                    engine = new Engine(engineInfo[0], int.Parse(engineInfo[1]), int.Parse(engineInfo[2]), engineInfo[3]);
                     engineList.Add(engine);
                 }
                 else if (engineInfo.Length == 2)
                 {
-                    Engine engine = new Engine(engineInfo[0], int.Parse(engineInfo[1]));
+                    engine = new Engine(engineInfo[0], int.Parse(engineInfo[1]));
                     engineList.Add(engine);
                 }
                 else if (engineInfo.Length == 3)
@@ -29,34 +31,74 @@
                     int displacement;
                     if (int.TryParse(engineInfo[2], out displacement))
                     {
-                        Engine engine = new Engine(engineInfo[0], int.Parse(engineInfo[1]), displacement);
+                        engine = new Engine(engineInfo[0], int.Parse(engineInfo[1]), displacement);
                         engineList.Add(engine);
                     }
                     else
                     {
-                        Engine engine = new Engine(engineInfo[0], int.Parse(engineInfo[1]), engineInfo[2]);
+                        engine = new Engine(engineInfo[0], int.Parse(engineInfo[1]), engineInfo[2]);
                         engineList.Add(engine);
                     }
                 }
             }
-            foreach (var engine in engineList)
+            int m = int.Parse(Console.ReadLine());
+
+            for (int j = 0; j < m; j++)
             {
-                if (engine.Efficiency == null)
+                string[] carInfo = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                //"{model} {engine} {weight} {color}"
+
+                engine = engineList.Find(x => x.Model == carInfo[1]);
+
+                if (carInfo.Length == 4)
                 {
-                    Console.WriteLine($"{engine.Model} {engine.Power} {engine.Displacement} n/a");
+                    Car car = new Car(carInfo[0], engine, int.Parse(carInfo[2]), carInfo[3]);
+                    carList.Add(car);
                 }
-                else if (engine.Displacement == 0)
+                else if (carInfo.Length == 2)
                 {
-                    Console.WriteLine($"{engine.Model} {engine.Power} n/a {engine.Efficiency}");
+                    Car car = new Car(carInfo[0], engine);
+                    carList.Add(car);
                 }
-                else if (engine.Efficiency == null && engine.Displacement == 0)
+                else if (carInfo.Length == 3)
                 {
-                    Console.WriteLine($"{engine.Model} {engine.Power} n/a n/a");
+                    int displacement;
+                    if (int.TryParse(carInfo[2], out displacement))
+                    {
+                        Car car = new Car(carInfo[0], engine, displacement);
+                        carList.Add(car);
+                    }
+                    else
+                    {
+                        Car car = new Car(carInfo[0], engine, carInfo[2]);
+                        carList.Add(car);
+                    }
                 }
-                else
-                {
-                    Console.WriteLine($"{engine.Model} {engine.Power} {engine.Displacement} {engine.Efficiency}");
-                }
+            }
+
+            //foreach (var car in carList)
+            //{
+            //    if (engine.Efficiency == null)
+            //    {
+            //        Console.WriteLine($"{engine.Model} {engine.Power} {engine.Displacement} n/a");
+            //    }
+            //    else if (engine.Displacement == 0)
+            //    {
+            //        Console.WriteLine($"{engine.Model} {engine.Power} n/a {engine.Efficiency}");
+            //    }
+            //    else if (engine.Efficiency == null && engine.Displacement == 0)
+            //    {
+            //        Console.WriteLine($"{engine.Model} {engine.Power} n/a n/a");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine($"{engine.Model} {engine.Power} {engine.Displacement} {engine.Efficiency}");
+            //    }
+            //}
+            foreach (var car in carList)
+            {
+                Console.WriteLine(car.ToString());
             }
         }
     }
