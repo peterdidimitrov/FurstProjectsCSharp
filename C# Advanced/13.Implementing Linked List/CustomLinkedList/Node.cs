aec110ca2e855bc5842d562437b490faf05ccd7a
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-06 16:15:30 +0200

    Add Custom Linked List

---
+++ C# Advanced/13.Implementing Linked List/CustomLinkedList/Node.cs
@@ -0,0 +1,23 @@
+﻿namespace CustomLinkedList
+{
+    public class Node
+    {
+        public Node(int value, Node next = null, Node previous = null)
+        {
+            Value = value;
+            Previous = previous;
+            Next = next;
+        }
+
+        public int Value { get; set; }
+
+        public Node Next { get; set; }
+
+        public Node Previous { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Previous?.Value} <- {Value} -> {Next?.Value}";
+        }
+    }
+}
