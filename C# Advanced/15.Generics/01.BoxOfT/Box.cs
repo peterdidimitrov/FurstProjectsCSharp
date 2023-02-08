c3b55a81f4a27c1e309e1b2a017e2fff9ed7cbc4
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-08 21:52:01 +0200

    Add first generics class and methods

---
+++ C# Advanced/15.Generics/01.BoxOfT/Box.cs
@@ -0,0 +1,27 @@
+﻿namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private int count;
+        private List<T> list;
+        public Box()
+        {
+            list = new List<T>();
+        }
+        public int Count { get { return count; } }
+        
+        public void Add(T element)
+        { 
+            count++;
+            list.Add(element);
+        }
+        public T Remove()
+        {
+            count--;
+            T removedElement = list[list.Count - 1];
+            list.Remove(removedElement);
+
+            return removedElement;
+        }
+    }
+}
---
+++ C# Advanced/15.Generics/01.BoxOfT/StartUp.cs
@@ -0,0 +1,19 @@
+﻿namespace BoxOfT
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new();
+            Box <int> box = new();
+            box.Add(1);
+            box.Add(2);
+            box.Add(3);
+
+            Console.WriteLine(box.Remove());
+            box.Add(4);
+            box.Add(5);
+            Console.WriteLine(box.Remove());
+        }
+    }
+}
