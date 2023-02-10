5860cfe5ddb72367d3f15c0406f853dad62997d5
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-10 11:57:07 +0200

    Add Generic Box of String solution

---
+++ C# Advanced/16.ExerciseGenerics/01.GenericBoxOfString/Box.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+
+namespace GenericBoxOfString
+{
+    public class Box<T>
+    {
+        private List<T> items; //field
+        
+        //constructor
+        public Box()
+        {
+            items = new List<T>();
+        }
+        public void Add(T item)
+        {
+            items.Add(item);
+        }
+        override public string ToString()
+        {
+            StringBuilder sb = new();
+
+            foreach (var item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
---
+++ C# Advanced/16.ExerciseGenerics/01.GenericBoxOfString/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericBoxOfString
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Box<string> box = new();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                box.Add(input);
+            }
+            Console.WriteLine(box.ToString());
+        }
+    }
+}
