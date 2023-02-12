8bf9647f20e3b38d298ad2b69ceebe6459033115
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-12 22:22:43 +0200

    Add more solutions

---
+++ C# Advanced/18.IteratorsAndComparators-Exercise/04.Froggy/StartUp.cs
@@ -0,0 +1,19 @@
+﻿using Froggy;
+
+namespace _04.Froggy
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<int> stones = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            Lake lake = new(stones);
+
+            Console.WriteLine(string.Join(", ", lake));
+        }
+    }
+}
---
+++ C# Advanced/18.IteratorsAndComparators-Exercise/05.ComparingObjects/StartUp.cs
@@ -0,0 +1,29 @@
+﻿using ComparingObjects;
+
+namespace 
+    ComparingObjects
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new();
+
+            string command = string.Empty;
+
+            while ((command = Console.ReadLine()) != "END")
+            {
+                string[] personProps = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                Person person = new()
+                {
+                    Name = personProps[0],
+                    Age = int.Parse(personProps[1]),
+                    Town = personProps[2]
+                };
+
+                people.Add(person);
+            }
+        }
+    }
+}
