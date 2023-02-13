f0045d56cdb8c4464a355ce2a5c64e1584e85b9a
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-13 22:38:23 +0200

    Add more solutions

---
+++ C# Advanced/04.MultidimensionalArrays-Exercise/8.Bombs/Program.cs
@@ -0,0 +1,136 @@
+﻿namespace _8.Bombs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = ReadMatrix<int>(
+                () => Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray(), size, size);
+
+            string[] coordinates = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            //Console.WriteLine(string.Join(" ", coordinates));
+
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                int[] indexies = coordinates[i]
+                    .Split(",")
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int row = indexies[0];
+                int col = indexies[1];
+
+                if (IsValid(row, col, matrix) &&
+                    matrix[row, col] > 0)
+                {
+                    int bombDamage = matrix[row, col];
+                    matrix = Explosion(bombDamage, row, col, matrix);
+                }
+            }
+            List<int> activeCells = FindActiveCells(matrix);
+            Console.WriteLine($"Alive cells: {activeCells.Count}");
+            Console.WriteLine($"Sum: {activeCells.Sum()}");
+
+            PrintMatrix(matrix, e => Console.Write(e + " "));
+        }
+        static T[,] ReadMatrix<T>(Func<T[]> rowDataGetter, int rows, int cols)
+        {
+            T[,] matrix = new T[rows, cols];
+            for (int row = 0; row < rows; row++)
+            {
+                T[] rowData = rowDataGetter();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = rowData[col];
+                }
+            }
+
+            return matrix;
+        }
+        static void PrintMatrix<T>(T[,] matrix, Action<T> printer)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    printer(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+        private static List<int> FindActiveCells(int[,] squareMatrix)
+        {
+            List<int> activeCells = new List<int>();
+
+            for (int row = 0; row < squareMatrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < squareMatrix.GetLength(1); col++)
+                {
+                    if (squareMatrix[row, col] > 0)
+                    {
+                        activeCells.Add(squareMatrix[row, col]);
+                    }
+                }
+            }
+
+            return activeCells;
+        }
+
+        private static int[,] Explosion(int bombDamage, int currentRowBomb, int currentColBomb, int[,] squareMatrix)
+        {
+            if (IsValid(currentRowBomb - 1, currentColBomb, squareMatrix) && squareMatrix[currentRowBomb - 1, currentColBomb] > 0)
+            {
+                squareMatrix[currentRowBomb - 1, currentColBomb] -= bombDamage;
+            }
+            if (IsValid(currentRowBomb + 1, currentColBomb, squareMatrix) && squareMatrix[currentRowBomb + 1, currentColBomb] > 0)
+            {
+                squareMatrix[currentRowBomb + 1, currentColBomb] -= bombDamage;
+            }
+            if (IsValid(currentRowBomb - 1, currentColBomb - 1, squareMatrix) && squareMatrix[currentRowBomb - 1, currentColBomb - 1] > 0)
+            {
+                squareMatrix[currentRowBomb - 1, currentColBomb - 1] -= bombDamage;
+            }
+            if (IsValid(currentRowBomb - 1, currentColBomb + 1, squareMatrix) && squareMatrix[currentRowBomb - 1, currentColBomb + 1] > 0)
+            {
+                squareMatrix[currentRowBomb - 1, currentColBomb + 1] -= bombDamage;
+            }
+            if (IsValid(currentRowBomb + 1, currentColBomb - 1, squareMatrix) && squareMatrix[currentRowBomb + 1, currentColBomb - 1] > 0)
+            {
+                squareMatrix[currentRowBomb + 1, currentColBomb - 1] -= bombDamage;
+            }
+            if (IsValid(currentRowBomb + 1, currentColBomb + 1, squareMatrix) && squareMatrix[currentRowBomb + 1, currentColBomb + 1] > 0)
+            {
+                squareMatrix[currentRowBomb + 1, currentColBomb + 1] -= bombDamage;
+            }
+            if (IsValid(currentRowBomb, currentColBomb - 1, squareMatrix) && squareMatrix[currentRowBomb, currentColBomb - 1] > 0)
+            {
+                squareMatrix[currentRowBomb, currentColBomb - 1] -= bombDamage;
+            }
+            if (IsValid(currentRowBomb, currentColBomb + 1, squareMatrix) && squareMatrix[currentRowBomb, currentColBomb + 1] > 0)
+            {
+                squareMatrix[currentRowBomb, currentColBomb + 1] -= bombDamage;
+            }
+
+            squareMatrix[currentRowBomb, currentColBomb] -= bombDamage;
+
+            return squareMatrix;
+        }
+
+        private static bool IsValid(int row, int col, int[,] squareMatrix)
+        {
+            if (row < squareMatrix.GetLength(0) && row >= 0 && col >= 0 && col < squareMatrix.GetLength(1))
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ C# Advanced/ExamPreparation/01.CScharpAdvancedRetakeExam14December2022/02.NavyBattle/Program.cs
@@ -0,0 +1,106 @@
+﻿namespace _02.NavyBattle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int battlefieldSize = int.Parse(Console.ReadLine());
+
+            string[,] battleField = new string[battlefieldSize, battlefieldSize];
+
+            int submRow = -1;
+            int submCol = -1;
+
+            int cruisersHit = 0;
+            int minesHit = 0;
+
+            for (int i = 0; i < battlefieldSize; i++)
+            {
+                string rowInput = Console.ReadLine();
+                for (int j = 0; j < battlefieldSize; j++)
+                {
+                    battleField[i, j] = rowInput[j].ToString();
+                    if (rowInput[j] == 'S')
+                    {
+                        battleField[i, j] = "-";
+                        submRow = i;
+                        submCol = j;
+                    }
+                }
+            }
+
+            while (true)
+            {
+                if (minesHit == 3)
+                {
+                    Console.WriteLine($"Mission failed, U-9 disappeared! Last known coordinates [{submRow}, {submCol}]!");
+                    PrintMatrix(battleField, e => Console.Write(e));
+                    return;
+                }
+                if (cruisersHit == 3)
+                {
+                    Console.WriteLine($"Mission accomplished, U-9 has destroyed all battle cruisers of the enemy!");
+                    PrintMatrix(battleField, e => Console.Write(e)); 
+                    return;
+                }
+                string command = Console.ReadLine().ToLower();
+                if (command == "right")
+                {
+                    Move(battleField, ref submRow, ref submCol, 0, 1, ref cruisersHit, ref minesHit);
+                }
+                else if (command == "left")
+                {
+                    Move(battleField, ref submRow, ref submCol, 0, -1, ref cruisersHit, ref minesHit);
+                }
+                else if (command == "up")
+                {
+                    Move(battleField, ref submRow, ref submCol, - 1, 0, ref cruisersHit, ref minesHit);
+                }
+                else if (command == "down")
+                {
+                    Move(battleField, ref submRow, ref submCol, 1, 0, ref cruisersHit, ref minesHit);
+                }
+            }
+        }
+
+        private static void Move(string[,] battleField, ref int submRow, ref int submCol, int row, int col, ref int cruisersHit, ref int minesHit)
+        {
+            if (battleField[submRow + row, submCol + col] == "-")
+            {
+                battleField[submRow, submCol] = "-";
+                submRow += row;
+                submCol += col;
+                battleField[submRow, submCol] = "S";
+            }
+            else if (battleField[submRow + row, submCol + col] == "*")
+            {
+                battleField[submRow, submCol] = "-";
+                submRow += row;
+                submCol += col;
+                minesHit++;
+                battleField[submRow, submCol] = "S";
+            }
+            else if (battleField[submRow + row, submCol + col] == "C")
+            {
+                battleField[submRow, submCol] = "-";
+                submRow += row;
+                submCol += col;
+                battleField[submRow, submCol] = "S";
+                cruisersHit++;
+            }
+        }
+
+        static void PrintMatrix<T>(T[,] matrix, Action<T> printer)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    printer(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
