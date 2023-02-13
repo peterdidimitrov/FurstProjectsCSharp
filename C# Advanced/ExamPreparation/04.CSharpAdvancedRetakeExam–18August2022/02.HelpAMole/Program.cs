b5a176dc882b8228ff68731ddac7105bca898c10
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-13 14:42:34 +0200

    Add more solutions

---
+++ C# Advanced/ExamPreparation/04.CSharpAdvancedRetakeExam–18August2022/02.HelpAMole/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace _02.HelpAMole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            int cols = int.Parse(Console.ReadLine());
+
+            int[,] matrix = ReadMatrix<int>(
+                () => Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray(), rows, cols);
+
+
+            PrintMatrix(matrix, e => Console.Write(e + " "));
+
+            static T[,] ReadMatrix<T>(Func<T[]> rowDataGetter, int rows, int cols)
+            {
+                T[,] matrix = new T[rows, cols];
+                for (int row = 0; row < rows; row++)
+                {
+                    T[] rowData = rowDataGetter();
+
+                    for (int col = 0; col < cols; col++)
+                    {
+                        matrix[row, col] = rowData[col];
+                    }
+                }
+
+                return matrix;
+            }
+
+            static void PrintMatrix<T>(T[,] matrix, Action<T> printer)
+            {
+                for (int row = 0; row < matrix.GetLength(0); row++)
+                {
+                    for (int col = 0; col < matrix.GetLength(1); col++)
+                    {
+                        printer(matrix[row, col]);
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
