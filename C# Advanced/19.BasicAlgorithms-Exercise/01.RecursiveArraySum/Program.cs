3a7b6cc4d396339680274fc28d88f8bb6819b241
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-21 11:52:36 +0200

    Add some basic algorithms

---
+++ C# Advanced/19.BasicAlgorithms-Exercise/01.RecursiveArraySum/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace _01.RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split().Select(int.Parse).ToArray();
+
+            Console.WriteLine(Sum(input, 0));
+        }
+        private static int Sum(int[] array, int index)
+        {
+            if (index >= array.Length)
+            {
+                return 0;
+            }
+
+            return array[index] + Sum(array, index + 1);
+        }
+    }
+}
---
+++ C# Advanced/19.BasicAlgorithms-Exercise/03.SumOfCoins/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace SumOfCoins
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        }
+
+        public static Dictionary<int, int> ChooseCoins(IList<int> coins, int targetSum)
+        {
+            return null;
+        }
+    }
+}
