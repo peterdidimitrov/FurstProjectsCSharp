af6daa100597f29b381e7e5fd7baba262a0e79e4
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-22 12:00:19 +0200

    Add two simple sorting algorithms

---
+++ C# Advanced/19.BasicAlgorithms-Exercise/06.QuickSort/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _06.QuickSort
+{
+    public class Quick
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            SortArray(input, 0, input.Length - 1);
+
+            Console.WriteLine(string.Join(" ", input));
+        }
+        public static int[] SortArray(int[] array, int leftIndex, int rightIndex)
+        {
+            var i = leftIndex;
+            var j = rightIndex;
+            var pivot = array[leftIndex];
+            while (i <= j)
+            {
+                while (array[i] < pivot)
+                {
+                    i++;
+                }
+
+                while (array[j] > pivot)
+                {
+                    j--;
+                }
+                if (i <= j)
+                {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                    i++;
+                    j--;
+                }
+            }
+
+            if (leftIndex < j)
+                SortArray(array, leftIndex, j);
+            if (i < rightIndex)
+                SortArray(array, i, rightIndex);
+            return array;
+        }
+    }
+}
