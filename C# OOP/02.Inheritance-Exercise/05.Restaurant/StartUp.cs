915bc42482b34063948203d787538f1333394ce4
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-24 16:52:33 +0200

    Add more solutions

---
+++ C# OOP/02.Inheritance-Exercise/05.Restaurant/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //Cake cake = new("Cake");
+            //Cake cake2 = new("Lava");
+            
+            //Console.WriteLine(cake.Price);
+            //Console.WriteLine(cake.Calories);
+        }
+    }
+}
