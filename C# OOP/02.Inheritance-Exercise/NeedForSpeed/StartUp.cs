972350a62676213b7124af03f7d6661e819f7a22
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-24 13:34:28 +0200

    Add one more solution - Inheritance

--- C# OOP/02.Inheritance-Exercise/NeedForSpeed/StartUp.cs
+++ C# OOP/02.Inheritance-Exercise/NeedForSpeed/StartUp.cs
@@ -1,10 +1,19 @@
-﻿namespace NeedForSpeed
+﻿using System;
+
+namespace NeedForSpeed
 {
     public class StartUp
     {
         public static void Main(string[] args)
         {
-            
+            //Motorcycle motor = new(10, 10);
+            //Console.WriteLine(motor.Fuel);
+            //Car car = new(100, 8.5);
+
+            //Console.WriteLine(car.Fuel);
+            //Console.WriteLine(motor.Fuel);
+            //CrossMotorcycle cross = new(100, 30);
+            //Console.WriteLine(cross.FuelConsumption);
         }
     }
 }
---
+++ C# OOP/02.Inheritance-Exercise/NeedForSpeed/Vehicle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeedForSpeed
+{
+    public abstract class Vehicle
+    {
+        private const double DefaultFuelConsumption = 1.25;
+        private int housePower;
+        private double fuel;
+
+        public Vehicle(int horsePower, double fuel)
+        {
+            HorsePower = horsePower;
+            Fuel = fuel;
+        }
+
+        public int HorsePower
+        {
+            get { return housePower; }
+            set { housePower = value; }
+        }
+        public double Fuel
+        {
+            get { return fuel; }
+            set { fuel = value; }
+        }
+
+        public virtual double FuelConsumption => DefaultFuelConsumption;
+        
+        public virtual void Drive(double kilometers)
+        {
+            Fuel -= kilometers * FuelConsumption;
+        }
+    }
+}
