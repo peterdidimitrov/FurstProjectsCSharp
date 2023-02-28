8e113b6b913e9ad17e54340582690cdcb0246378
peterdidimitrov <petr_dimitrov@abv.bg>
2023-02-28 11:33:56 +0200

    Add box's solution - Exercise Encapsulation

---
+++ C# OOP/04.ExerciseEncapsulation/01.ClassBoxData/Box.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassBoxData
+{
+    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get { return length; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+        public double Width
+        {
+            get { return width; }
+            private set 
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Width cannot be zero or negative.");
+                }
+                width = value; 
+            }
+        }
+        public double Height
+        {
+            get { return height; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException($"Height cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+        public double SurfaceArea(Box box)
+        {
+            return 2 * (box.width * box.length + box.length * box.height + box.width * box.height);
+        }
+        public double LateralSurfaceArea(Box box)
+        {
+            return 2 * box.height * (box.width + box.length);
+        }
+        public double Volume(Box box)
+        {
+            return box.height * box.width * box.length;
+        }
+    }
+}
