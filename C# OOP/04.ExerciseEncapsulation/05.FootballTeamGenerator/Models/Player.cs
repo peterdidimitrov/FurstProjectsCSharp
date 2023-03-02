d43d552929449f812979d41f90ea08fcd328241e
peterdidimitrov <petr_dimitrov@abv.bg>
2023-03-02 11:49:39 +0200

    Add new solution

---
+++ C# OOP/04.ExerciseEncapsulation/05.FootballTeamGenerator/Models/Player.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FootballTeamGenerator.Models
+{
+    public class Player
+    {
+        private string name;
+        private Stats stats;
+
+        public string Name
+        {
+            get { return name; }
+            private set { name = value; }
+        }
+        public Stats Stats
+        {
+            get { return stats; }
+            private set { stats = value; }
+        }
+        private int GetSkilLevel() => (stats.Endurance + stats.Dribble + stats.Passing + stats.Sprint + stats.Shooting) / 5;
+
+    }
+}
--- C# OOP/04.ExerciseEncapsulation/05.FootballTeamGenerator/Models/Team .cs
+++ C# OOP/04.ExerciseEncapsulation/05.FootballTeamGenerator/Models/Team .cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FootballTeamGenerator
+namespace FootballTeamGenerator.Models
 {
     public class Team
     {
