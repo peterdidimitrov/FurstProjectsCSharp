08aaac221ca9684d4c36aedbff84b5b97954ddc9
peterdidimitrov <petr_dimitrov@abv.bg>
2023-03-02 11:05:37 +0200

    Add final solution of the Pizza Calories problem

--- C# OOP/04.ExerciseEncapsulation/04.PizzaCalories/StartUp.cs
+++ C# OOP/04.ExerciseEncapsulation/04.PizzaCalories/StartUp.cs
@@ -1,30 +1,36 @@
-﻿namespace PizzaCalories
+﻿using PizzaCallories.Models;
+
+try
 {
-    public class StartUp
+    //missing name should throw custom exception - not using RemoveEmptyEntries
+    string[] pizzaTokens = Console.ReadLine().Split();
+    string[] doughTokens = Console.ReadLine().Split();
+
+    string pizzaName = pizzaTokens[1];
+
+    Dough dough = new(doughTokens[1], doughTokens[2], double.Parse(doughTokens[3]));
+
+    Pizza pizza = new(pizzaName, dough);
+
+    while (true)
     {
-        static void Main(string[] args)
+        string toppingsInput = Console.ReadLine();
+
+        if (toppingsInput == "END")
         {
-            try
-            {
-                string commands;
-
-                while ((commands = Console.ReadLine().ToLower()) != "end")
-                {
-                    string[] commandArguments = commands
-                        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-                    string doughType = commandArguments[1];
-                    string bakingTechnique = commandArguments[2];
-                    double weight = double.Parse(commandArguments[3]);
-
-                    Dough dough = new Dough(doughType, bakingTechnique, weight);
-                    Console.WriteLine(dough.GetCalories());
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
+            break;
         }
+
+        string[] toppingsTokens = toppingsInput.Split();
+
+        Topping topping = new(toppingsTokens[1], double.Parse(toppingsTokens[2]));
+
+        pizza.AddTopping(topping);
     }
+
+    Console.WriteLine(pizza);
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
 }
