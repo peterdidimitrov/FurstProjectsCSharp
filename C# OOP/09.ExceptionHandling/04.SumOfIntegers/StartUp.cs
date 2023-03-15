b0ad1703d92bca4b35196bfcd6f29de56c90b293
peterdidimitrov <petr_dimitrov@abv.bg>
2023-03-15 23:57:56 +0200

    Add new solution

---
+++ C# OOP/09.ExceptionHandling/04.SumOfIntegers/StartUp.cs
@@ -0,0 +1,47 @@
+﻿using System.Numerics;
+
+namespace SumOfIntegers
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string[] elements = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            BigInteger sum = 0;
+
+            for (int i = 0; i < elements.Length; i++)
+            {
+                int num;
+                try
+                {
+                    if (int.Parse(elements[i]) < int.MinValue || int.Parse(elements[i]) > int.MaxValue)
+                    {
+                        throw new OverflowException();
+                    }
+                    if (!int.TryParse(elements[i], out num))
+                    {
+                        throw new FormatException();
+                    }
+                    num = int.Parse(elements[i]);
+
+                    sum += num;
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{elements[i]}' is out of range!");
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine($"The element '{elements[i]}' is in wrong format!");
+                }
+                finally
+                {
+                    Console.WriteLine($"Element '{elements[i]}' processed - current sum: {sum}");
+                }
+            }
+            Console.WriteLine($"The total sum of all integers is: {sum}");
+        }
+    }
+}
