fcd1f2ba7223ec6d228e8a7078c90054f27304cf
peterdidimitrov <petr_dimitrov@abv.bg>
2023-03-16 23:49:25 +0200

    Change solution of the problem - Sum of Integers - ExceptionHandiling

--- C# OOP/09.ExceptionHandling/04.SumOfIntegers/StartUp.cs
+++ C# OOP/09.ExceptionHandling/04.SumOfIntegers/StartUp.cs
@@ -7,34 +7,30 @@ namespace SumOfIntegers
         static void Main(string[] args)
         {
             string[] elements = Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                .Split(' '); // Don't use StringSplitOption.RemoveEmptyEntries. Judge didn't expect it.
 
             BigInteger sum = 0;
 
             for (int i = 0; i < elements.Length; i++)
             {
-                int num;
                 try
                 {
                     if (int.Parse(elements[i]) < int.MinValue || int.Parse(elements[i]) > int.MaxValue)
                     {
                         throw new OverflowException();
                     }
-                    if (!int.TryParse(elements[i], out num))
-                    {
-                        throw new FormatException();
-                    }
-                    num = int.Parse(elements[i]);
+
+                    int num = int.Parse(elements[i]);
 
                     sum += num;
-                }
-                catch (OverflowException)
-                {
-                    Console.WriteLine($"The element '{elements[i]}' is out of range!");
                 }
                 catch (FormatException)
                 {
                     Console.WriteLine($"The element '{elements[i]}' is in wrong format!");
+                }
+                catch (OverflowException)
+                {
+                    Console.WriteLine($"The element '{elements[i]}' is out of range!");
                 }
                 finally
                 {
