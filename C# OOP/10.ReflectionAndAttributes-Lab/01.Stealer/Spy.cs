c2a150883fd9fd8fd01c419298750d4d2d944fe6
peterdidimitrov <petr_dimitrov@abv.bg>
2023-03-17 14:31:55 +0200

    Add more solutions

---
+++ C# OOP/10.ReflectionAndAttributes-Lab/01.Stealer/Spy.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Reflection;
+using System.Text;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string nameOfClass, params string[] fieldsNames)
+        {
+            Type classType = Type.GetType(nameOfClass);
+
+            FieldInfo[] fields = classType.GetFields(BindingFlags.Public | BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Static);
+
+            StringBuilder srtingBuilder = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType, new Object[] { });
+
+            srtingBuilder.AppendLine($"Class under investigation: {nameOfClass}");
+
+            foreach (var field in fields.Where(f => fieldsNames.Contains(f.Name)))
+            {
+                srtingBuilder.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+            return srtingBuilder.ToString().Trim();
+        }
+    }
+}
