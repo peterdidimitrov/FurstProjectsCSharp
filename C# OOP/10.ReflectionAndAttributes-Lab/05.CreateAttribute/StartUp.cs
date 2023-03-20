64936c7945fba04c11072207c652cf4386a609b5
peterdidimitrov <petr_dimitrov@abv.bg>
2023-03-20 23:46:53 +0200

    Add more solutions

---
+++ C# OOP/10.ReflectionAndAttributes-Lab/05.CreateAttribute/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    public static class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+            var traker = new Tracker();
+            traker.PrintMethodsByAuthor();
+        }
+
+    }
+}
