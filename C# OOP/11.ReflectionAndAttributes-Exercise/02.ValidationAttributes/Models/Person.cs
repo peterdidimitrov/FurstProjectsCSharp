ae0263737b57f23c2beb2287def893b19d44b6ee
peterdidimitrov <petr_dimitrov@abv.bg>
2023-03-21 23:13:02 +0200

    Add solution of Validation Attributes

---
+++ C# OOP/11.ReflectionAndAttributes-Exercise/02.ValidationAttributes/Models/Person.cs
@@ -0,0 +1,24 @@
+﻿
+
+using ValidationAttributes.Utilities;
+using ValidationAttributes.Utilities.Attributes;
+
+namespace ValidationAttributes.Models
+{
+    public class Person
+    {
+        private const int AgeMinValue = 12;
+        private const int AgeMaxValue = 90;
+
+        public Person(string fullName, int age)
+        {
+            FullName = fullName;
+            Age = age;
+        }
+
+        [MyRequired]
+        public string FullName { get; private set; }
+        [MyRange(AgeMinValue, AgeMaxValue)]
+        public int Age { get; private set; }
+    }
+}
